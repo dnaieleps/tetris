8ba846c9a06560826cc8c34477e024723652570e
dnaieleps <dan.c.espiritu@gmail.com>
2025-04-27 01:20:44 -0700

    continued adding things to main game board

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Text.hpp>
 #include <string>
+#include <queue>
 
+/******* COLOR CONSTANT DEFINITIONS *******/
 #define GRAY sf::Color(57, 63, 78, 255)                      // board windows and sub-windows
 #define LIGHT_BLUE sf::Color(82, 195, 225, 255)              // I piece
 #define YELLOW sf::Color(226, 241, 97, 255)                  // O piece
@@ -10,6 +14,7 @@
 #define RED sf::Color(225, 38, 38, 255)                      // Z piece
 #define GREEN sf::Color(33, 172, 42, 255)                    // S piece
 #define PURPLE sf::Color(104, 61, 212, 255)                  // T piece
+
 
 int main()
 {
@@ -29,18 +34,29 @@ int main()
     next3pieces->setFillColor(GRAY);
     next3pieces->setPosition({375, 250});
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
+    sf::RectangleShape* pauseButton = new sf::RectangleShape({50, 30});
+    pauseButton->setFillColor(GRAY); 
+    pauseButton->setPosition({30, 25});
+
+    sf::Font arial("arial.ttf");
+    sf::Text* score = new sf::Text(arial, "Score: ", 25);
+    score->setString("Score: ");
+    score->setPosition({260, 25});
+
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
         
         window.clear();
+
         window.draw(*board);
         window.draw(*nextpiece);
         window.draw(*next3pieces);
+        window.draw(*pauseButton);
+        window.draw(*score);
 
         window.display();
     }
